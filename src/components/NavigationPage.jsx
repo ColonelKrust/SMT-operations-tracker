316c63033cb5288c260ffd45e1d9026840061f02
Dakota Lust <lustdakota14@gmail.com>
2024-12-20 21:03:47 -0700

    initial styling for home page title and buttons

--- src/components/NavigationPage.jsx
+++ src/components/NavigationPage.jsx
@@ -1,15 +1,19 @@
-import React from "react";
-import { NavLink } from "react-router";
+import React from 'react';
+import { NavLink } from 'react-router';
+import '../styles/nav-page.css';
 
 export default function NavigationPage() {
     return(
         <div id='navPageDiv'>
-            <NavLink to='/runtimeData'>
-                <button id='navPageDataButton' className='navPageButtons'>Runtime Data</button>
-            </NavLink>
-            <NavLink to='/newRunForm'>
-                <button id='navPageNewRunButton' className='navPageButtons'>New Run</button>
-            </NavLink>
+            <h1 id='navPageTitle'>SMT Line Tracker</h1>
+            <div id='navButtonsDiv'>
+                <NavLink to='/runtimeData'>
+                    <button id='navPageDataButton' className='navPageButtons'>Runtime Data</button>
+                </NavLink>
+                <NavLink to='/newRunForm'>
+                    <button id='navPageNewRunButton' className='navPageButtons'>New Run</button>
+                </NavLink>
+            </div>
         </div>
     )
 }
